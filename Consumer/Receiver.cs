051224db64dfbeeb10e31de82672c12587cbba55
Eric Alan Rodriguez Contreras <licalan.sistemas@gmail.com>
2025-01-29 10:15:30 -0600

    Agregar archivos de proyecto.

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+var connection = await factory.CreateConnectionAsync();
+using (connection)
+using (var channel = await connection.CreateChannelAsync())
+{
+    await channel.QueueDeclareAsync(
+        queue: "BasicTest",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null);
+
+    var consumer = new RabbitMQ.Client.Events.AsyncEventingBasicConsumer(channel);
+    consumer.ReceivedAsync += async (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine(" [x] Received {0}", message);
+        await Task.CompletedTask;
+    };
+    await channel.BasicConsumeAsync(queue: "BasicTest", autoAck: true, consumer: consumer);
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
---
+++ Producer/Sender.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+var connection = await factory.CreateConnectionAsync();
+using (connection)
+using (var channel = await connection.CreateChannelAsync())
+{
+    await channel.QueueDeclareAsync(
+        queue: "BasicTest",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null);
+
+    string message = "Getting started with .Net Core RabbitMQ";
+    var body = Encoding.UTF8.GetBytes(message);
+    var properties = new BasicProperties();
+    await channel.BasicPublishAsync("", "BasicTest", false, properties, body);
+    Console.WriteLine(" [x] Sent {0}", message);
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
